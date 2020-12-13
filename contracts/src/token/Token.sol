bca87fb98c972e8ab612b724c6c560acd8457923
juztamau5 <juztamau5@gmail.com>
2020-12-13 14:40:36 -0800

    Isolate magic constants and add documentation
    Magic numbers/strings should be declared above their use to separate
    configuration and logic.

--- contracts/src/token/Token.sol
+++ contracts/src/token/Token.sol
@@ -10,11 +10,37 @@ pragma solidity >=0.7.0 <0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20Capped.sol';
 import '@openzeppelin/contracts/access/AccessControl.sol';
+
 import 'contracts/interfaces/uniswap/IUniswapV2Router02.sol';
 import 'contracts/interfaces/uniswap/IUniswapV2Factory.sol';
 import 'contracts/interfaces/uniswap/IUniswapV2Pair.sol';
 
 contract WolfToken is ERC20Capped, AccessControl {
+  /**
+   * @dev The ERC 20 token name used by wallets to identify the token
+   */
+  string private constant TOKEN_NAME = 'Wolf Token';
+
+  /**
+   * @dev The ERC 20 token symbol used as an abbreviation of the token, such
+   * as BTC, ETH, AUG or SJCX.
+   */
+  string private constant TOKEN_SYMBOL = 'WOW';
+
+  /**
+   * @dev The number of decimal places to which the token will be calculated.
+   * The most common number of decimals to consider is 18.
+   */
+  uint8 private constant TOKEN_DECIMALS = 18;
+
+  /**
+   * @dev 60.000 tokens maximal supply
+   */
+  uint256 private constant MAX_SUPPLY = 60000 * 1e18;
+
+  /**
+   * @dev Role to allow minting of new tokens
+   */
   bytes32 public constant MINTER_ROLE = 'minter_role';
 
   IUniswapV2Factory private constant _uniV2Factory =
@@ -23,44 +49,75 @@ contract WolfToken is ERC20Capped, AccessControl {
     IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
 
   address public immutable _uniV2Pair;
-  // if true, this pair is blocked
+
+  /**
+   * @dev If true, this pair is blocked
+   */
   mapping(address => bool) _uniV2Blacklist;
   uint256 _uniV2PairsScanned = _uniV2Factory.allPairsLength();
 
-  // We inherited the ERC20Capped.sol
+  /**
+   * @dev Construct a token instance
+   *
+   * @param _teamWallet The team wallet to receive initial supply
+   */
   constructor(address _teamWallet)
-    // 60.000 tokens maximal supply
-    ERC20Capped(60000 * 1e18)
-    ERC20('Wolf Token', 'WOLF')
+    ERC20Capped(MAX_SUPPLY)
+    ERC20(TOKEN_NAME, TOKEN_SYMBOL)
   {
-    /* mint 9875 into teams wallet
-       1.) 500 tokens * 15 month = 7500 team rewards
-       2.) 1375 token for development costs (audits / bug-bounty ...)
-       3.) 1000 token for marketing (influencer / design ...)
-    */
+    // Initialize ERC20 base
+    _setupDecimals(TOKEN_DECIMALS);
+
+    /*
+     * Mint 9875 into teams wallet
+     *
+     *   1.) 500 tokens * 15 month = 7500 team rewards
+     *   2.) 1375 token for development costs (audits / bug-bounty ...)
+     *   3.) 1000 token for marketing (influencer / design ...)
+     */
     _mint(_teamWallet, 9875 * 1e18);
+
     // Multi-sig teamwallet has initial admin rights, eg for adding minters
     _setupRole(DEFAULT_ADMIN_ROLE, _teamWallet);
+
     // Create the UniV2 liquidity pool
     address weth = _uniV2Router.WETH();
     _uniV2Pair = _uniV2Factory.createPair(address(this), weth);
     _updateUniV2Blacklist();
   }
 
-  // mint is only allowed by addresses with minter role
+  /**
+   * @dev Mint tokens
+   *
+   * @param account The account to receive the tokens
+   * @param amount The amount to mint
+   *
+   * @return True if successful, reverts on failure
+   */
   function mint(address account, uint256 amount) external returns (bool) {
-    require(hasRole(MINTER_ROLE, msg.sender));
+    // Mint is only allowed by addresses with minter role
+    require(hasRole(MINTER_ROLE, msg.sender), 'Only minters allowed');
+
     _mint(account, amount);
+
     return true;
   }
 
-  // remove ETH/WOLF univ2 pair address from blacklist
+  /**
+   * @dev Remove ETH/WOLF univ2 pair address from blacklist
+   *
+   * @param enable True to enable the univ2 pair, false to disable
+   */
   function enableUniV2Pair(bool enable) external {
     require(hasRole(MINTER_ROLE, msg.sender));
     _uniV2Blacklist[_uniV2Pair] = !enable;
   }
 
-  // remove univ2 pair address from blacklist
+  /**
+   * @dev Remove univ2 pair address from blacklist
+   *
+   * @param pairAddress The address of the univ2 pair
+   */
   function enableUniV2Pair(address pairAddress) external {
     require(
       hasRole(MINTER_ROLE, msg.sender) ||
@@ -69,13 +126,17 @@ contract WolfToken is ERC20Capped, AccessControl {
     _uniV2Blacklist[pairAddress] = false;
   }
 
-  // add univ2 pair address to blacklist
+  /**
+   * @dev Add univ2 pair address to blacklist
+   */
   function disableUniV2Pair(address pairAddress) external {
     require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
     _uniV2Blacklist[pairAddress] = true;
   }
 
-  // request the state of the univ2 pair address
+  /**
+   * @dev Request the state of the univ2 pair address
+   */
   function isUniV2PairEnabled(address pairAddress)
     external
     view
@@ -84,7 +145,9 @@ contract WolfToken is ERC20Capped, AccessControl {
     return !_uniV2Blacklist[pairAddress];
   }
 
-  // override to prevent creation of uniswap LP's with WOLF token
+  /**
+   * @dev Override to prevent creation of uniswap LP's with WOLF token
+   */
   function _transfer(
     address sender,
     address recipient,
