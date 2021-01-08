b68b9f180e6b7804074bf51c722cb4966aff7ab3
juztamau5 <juztamau5@gmail.com>
2021-01-08 14:40:23 -0800

    Add NVM dependency

---
+++ tools/depends/native/nvm/package.mk
@@ -0,0 +1,56 @@
+################################################################################
+#
+#  Copyright (C) 2020 The Wolfpack
+#  This file is part of wolves.finance - https://github.com/wolvesofwallstreet/wolves.finance
+#
+#  SPDX-License-Identifier: Apache-2.0
+#  See the file LICENSE.txt for more information.
+#
+################################################################################
+
+################################################################################
+#
+# Node Version Manager
+#
+# SPDX-License-Identifier: MIT
+#
+################################################################################
+
+# Dependency name and version
+NVM_REPO_NAME = nvm
+NVM_VERSION = 0.37.0
+NVM_INSTALL_SCRIPT = https://raw.githubusercontent.com/nvm-sh/$(NVM_REPO_NAME)/v$(NVM_VERSION)/install.sh
+NVM_BIN = nvm-exec
+
+################################################################################
+#
+# Paths
+#
+################################################################################
+
+# Checkout directory
+REPO_DIR_NVM = $(REPO_DIR)/$(NVM_REPO_NAME)
+
+# Downloaded binary
+NVM_BIN_PATH = $(REPO_DIR_NVM)/$(NVM_BIN)
+
+################################################################################
+#
+# Checkout
+#
+################################################################################
+
+$(NVM_BIN_PATH): $(S)/.precheckout
+	mkdir -p "$(REPO_DIR_NVM)"
+
+	[ -f "$(NVM_BIN_PATH)" ] || ( \
+	  wget -qO- "$(NVM_INSTALL_SCRIPT)" | \
+	    NVM_DIR="$(REPO_DIR_NVM)" \
+	    PROFILE="/dev/null" \
+	    bash \
+	)
+
+	touch "$@"
+
+$(S)/checkout-nvm: $(NVM_BIN_PATH)
+	touch "$@"
