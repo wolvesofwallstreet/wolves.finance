c10e287b6a2b0541999cb85f35d9c8b312a9494f
daniel cohen <daniel7777cohen@gmail.com>
2021-03-14 20:57:53 +0200

    styling for ticker

--- src/components/wowsstatus/wowsstatus.tsx
+++ src/components/wowsstatus/wowsstatus.tsx
@@ -15,8 +15,9 @@ class WowsStatus extends Component {
   render(): ReactNode {
     return (
       <ProgressStatus>
-        <span className="ticker-text">WOWS CURRENT PRICE: $34.45</span>
-        <span className="ticker-text">CIRCULATING MC: $450,000</span>
+        <span className="ticker-text">
+          WOWS CURRENT PRICE: $34.45 CIRCULATING MC: $450,000
+        </span>
       </ProgressStatus>
     );
   }
