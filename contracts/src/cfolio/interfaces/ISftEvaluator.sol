80cfdcd628787b0f4e69730534bf978bd87e0c2f
peak3d <peak3d@videvago.com>
2021-04-15 14:22:28 +0200

    [SOL] TFC: fix transfer on burn / interface modifications

---
+++ contracts/src/cfolio/interfaces/ISftEvaluator.sol
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2021 The Wolfpack
+ * This file is part of wolves.finance - https://github.com/wolvesofwallstreet/wolves.finance
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * See LICENSE.txt for more information.
+ */
+
+pragma solidity >=0.7.0 <0.8.0;
+
+interface ISftEvaluator {
+  function calculateRate(uint256 sftTokenId) external view returns(uint32);
+}
--- contracts/src/cfolio/interfaces/ITradefloorClient.sol
+++ contracts/src/cfolio/interfaces/ITradefloorClient.sol
@@ -13,11 +13,14 @@ import '../../token/interfaces/IMinterCallback.sol';
 /**
  * @dev Interface to C-folio item contracts
  */
-interface ITradefloorClient is IMinterCallback {
+interface ITradeFloorClient is IMinterCallback {
   /**
    * @dev Called when a SFT tokens grade needs re-evaluation
    *
    * @param tokenId The ERC-1155 token ID
+   * Rates in 1E6 convention: 1E6 = 100%
+   * @param prevRate previous value rate
+   * @param newRate new value rate
    */
-  function upgrade(uint256 tokenId) external;
+  function sftUpgrade(uint256 tokenId, uint32 prevRate, uint32 newRate) external;
 }
