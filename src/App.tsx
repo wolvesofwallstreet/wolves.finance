f3e12de9cd8a1826aae8c2bf126ed0547eb6aa0b
Dipen parmar <dipensavji@gmail.com>
2021-05-06 18:42:21 +0530

    [UI] page-12 (C-FOLIO MANAGER) added   - URI `BASE_URL/c_folio_manager`

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ import Page7BoisBoardrooms from './components/page7';
 import Page8InvestmentSfts from './components/page8InvestmentSfts';
 import Page9BoisBoardrooms from './components/Page9BoisBoardrooms';
 import Page11 from './components/page11';
+import Page12CFolioManager from './components/page12';
 import { PageStatus } from './components/pageStatus';
 import Stake from './components/stake';
 import WolfToast from './components/toast/wolftoast';
@@ -61,6 +62,7 @@ class App extends React.Component {
               />
               <Route path="/yearn_invest" component={Page9BoisBoardrooms} />
               <Route path="/page11" component={Page11} />
+              <Route path="/c_folio_manager" component={Page12CFolioManager} />
               <Route component={Page1} />
             </Switch>
             <Footer />
