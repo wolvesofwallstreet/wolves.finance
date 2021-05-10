2ca4bda3416d4ee6d6b40d01abcf6b16da9ba433
Dipen Parmar <dipensavji@gmail.com>
2021-05-10 18:52:24 +0530

    [UI]  Header re-organized,     - Page9-(BoisBoardrooms) converted to Class Component     - fixed some responsive design issues

--- src/components/page3TradeFloor/Page3TradeFloor.tsx
+++ src/components/page3TradeFloor/Page3TradeFloor.tsx
@@ -159,9 +159,7 @@ class Page3TradeFloor extends Component<PAGE7_PROPS, PAGE7_STATE> {
       cards.map((card, index) => {
         return (
           <div className="page3Trade-card" key={index + Math.random()}>
-            <Link
-              to={`detail?type=wolves&levelId=${card?.levelId}&cardId=${card.cardId}`}
-            >
+            <Link to={`?item=${index}`}>
               <video
                 className="card-media"
                 src={card.image}
