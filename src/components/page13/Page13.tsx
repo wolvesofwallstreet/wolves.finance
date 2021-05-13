9704e1c6b9039c9b164683334459ad64d38c3c6e
Dipen Parmar <dipensavji@gmail.com>
2021-05-13 15:16:49 +0530

    [UI] page13 improved responsiveness

--- src/components/page13/Page13.tsx
+++ src/components/page13/Page13.tsx
@@ -45,7 +45,7 @@ function Page13({ t, location }: PROPS) {
           </h3>
         </div>
 
-        <div className="w-75 center-container">
+        <div className="page13_container_secondary center-container">
           <div
             className={
               'd-flex flex-column flex-md-row justify-content-center bg-blue-transparent font-14 py-2 px-3 my-2 mt-3 '
@@ -56,61 +56,53 @@ function Page13({ t, location }: PROPS) {
             <span className={' m-1 mr-1'}> 12 DAI = $303.00 TOTAL</span>
           </div>
 
-          <div className={'page13-d-grid text-left mb-4'}>
-            {[1].map((card, i) => {
-              return (
-                <div
-                  className="px-4 py-3 bg-blue-transparent"
-                  key={i + Math.random()}
-                >
-                  <div
-                    className={
-                      'd-flex flex-lg-row flex-md-row  flex-xs-column justify-content-between flex-wrap '
-                    }
-                  >
+          <div className={'page13_container_secondary text-left mb-4'}>
+            <div className="px-4 py-2 bg-blue-transparent " key={'p13'}>
+              <div className={'page14-card justify-content-between'}>
+                <div className="p-1">
+                  <div className="d-flex justify-content-between flex-column">
+                    <h3 className={'tk-vincente-bold font-24'}>
+                      BOIS LOS ANGELES DAI
+                    </h3>
                     <div
-                      className={'d-flex flex-column justify-content-between'}
+                      className={
+                        'd-flex justify-content-between flex-column flex-lg-row flex-md-row flex-sm-column flex-xs-column'
+                      }
                     >
-                      <div className="">
-                        <h3 className={'tk-vincente-bold font-32'}>
-                          BOIS LOS ANGELES DAI
-                        </h3>
-                        <div
-                          // className={'d-flex'}
-                          className={
-                            'd-flex flex-column flex-lg-row flex-sm-column flex-md-column flex-xs-column'
-                          }
-                        >
-                          <span className={'h-5 f-light single-line mt-2 mr-5'}>
-                            LPToken: 0.00
-                          </span>
-                          <span className={'h-5 f-light single-line mt-2 mr-5'}>
-                            Staked: 0.00
-                          </span>
-                          <span className={'h-5 f-light single-line mt-2 '}>
-                            Earned: 0.000000 WOWS
-                          </span>
-                        </div>
-                      </div>
-                      <div className={'pt-2 f-light h-5 '}>
-                        <span className="pr-2 "> APY*: 394.45% </span>
-                        <span className="pr-2 nowrap"> APR: 162.31% </span>
-                      </div>
-                    </div>
-                    <div className="">
-                      <img
-                        className={'responsive-img'}
-                        src={
-                          'https://4travelers.de/wolves_assets/cards/bois/level2/BRYANT-300.mp4.jpg'
+                      <span
+                        className={
+                          'font-14 f-light single-line mt-2 mr-3 text-nowrap'
                         }
-                        alt={'card img'}
-                        style={{ maxWidth: '160px', width: '100%' }}
-                      />
+                      >
+                        LPToken: 0.00
+                      </span>
+                      <span className={'font-14 f-light single-line mt-2 mr-3'}>
+                        Staked: 0.00
+                      </span>
+                      <span className={'font-14 f-light single-line mt-2'}>
+                        Earned: 0.000000 WOWS
+                      </span>
                     </div>
                   </div>
                 </div>
-              );
-            })}
+                <div className="">
+                  <img
+                    className={'img-fluid g-card-big-small border broder-1'}
+                    src={
+                      'https://4travelers.de/wolves_assets/cards/bois/level2/BRYANT-300.mp4.jpg'
+                    }
+                    alt={'bryant'}
+                    style={{ maxWidth: '120px', width: '100%' }}
+                  />
+                </div>
+              </div>
+              <div className={'pt-1 font-16'}>
+                <span className="font-14  pr-2 f-light"> APY*: 394.45% </span>
+                <span className="font-14  pr-2 f-light text-nowrap">
+                  APR: 162.31%
+                </span>
+              </div>
+            </div>
           </div>
         </div>
 
