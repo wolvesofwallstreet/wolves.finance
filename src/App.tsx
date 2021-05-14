3eb092e5d60fb1026071b1bc6dca4d66e11a941a
peak3d <peak3d@videvago.com>
2021-05-14 16:20:36 +0200

    [UI] Fix page9Bois / stack overflow

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import './App.css';
 import './wolves_scheme.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/theme/button/wolve_button.css';
-import './components/theme/nuka_carousel/nuka_carousel.css';
 import './components/theme/form/input/wolve_input.css';
 import './components/theme/glidejs/glide.core.min.css';
 import './components/theme/glidejs/glide.theme.min.css';
