2634ca8158ebc557d898894ec6edd72d6a1e5b65
peak3d <peak3d@videvago.com>
2021-05-15 16:05:50 +0200

    Add Playground files

---
+++ playground/infura.sh
@@ -0,0 +1,3 @@
+wscat wss://rinkeby.infura.io/v3/6ccfc6aa1fa44805a7f4a52a2fb51ef0\
+    < '{"id": 1, "method": "eth_subscribe", "params": ["newPendingTransactions"]}'
+#    -d '{"jsonrpc":"2.0","method":"eth_getTransactionByHash","params": ["0xb7a9dd1409e5c4a0600d4e140aa6433f1df8d794a2709682216fce9be8e1831f"],"id":0}'
