8937d73bca4f9fe8b361faea3830d7af10e7f4fc
juztamau5 <juztamau5@gmail.com>
2021-06-08 13:35:54 -0700

    [TEST] Add tests for SC NFTs (58 tests passing)

--- test/utils/constants.ts
+++ test/utils/constants.ts
@@ -6,6 +6,12 @@
  * See the file LICENSES/README.md for more information.
  */
 
+import { ethers } from 'ethers';
+
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
 
 export const HASH_MASK = '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF';
+
+export const MAX_UINT256 = ethers.BigNumber.from(
+  '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'
+);
